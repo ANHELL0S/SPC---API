f76f4164e2dc9c9fdf7a64bf4e3ae699a15c437e
Angelo García <161162882+ANHELL0S@users.noreply.github.com>
2024-06-26 09:51:17 -0500

    Update jwt.js

--- libs/jwt.js
+++ libs/jwt.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 export function createAccessToken(payload) {
 	return new Promise((resolve, reject) => {
-		jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '5h' }, (err, token) => {
+		jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' }, (err, token) => {
 			if (err) reject(err)
 			resolve(token)
 		})
